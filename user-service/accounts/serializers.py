ee6e830eb1dd329769a1f972b98f54fc8fd4f40f
Aqiba Abdul Qadir <aqibaabdulqadir@gmail.com>
2026-05-15 22:50:01 +0500

    Make Serialiser more comprehensive

--- user-service/accounts/serializers.py
+++ user-service/accounts/serializers.py
@@ -28,8 +28,8 @@ class LoginSerializer(serializers.Serializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'name', 'role', 'date_joined']
-        read_only_fields = ['id', 'email', 'date_joined']
+        fields = ['id', 'email', 'name', 'role', 'is_staff', 'is_superuser', 'date_joined']
+        read_only_fields = ['id', 'email', 'date_joined', 'is_staff', 'is_superuser']
 
 
 class GoogleLoginSerializer(serializers.Serializer):
